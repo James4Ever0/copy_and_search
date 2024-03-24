9b2ddc4d07125617b674e9e959a414444166f559
jamesbrown <randomvoidmail@foxmail.com>
2024-03-24 20:41:26 +0800

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 # conda run -n rosetta --no-capture-output pyinstaller --name 复制搜索 main.py 
 # conda run -n rosetta --no-capture-output pyinstaller --noupx --name 复制搜索 main.py 
 
-conda run -n rosetta --no-capture-output pyinstaller --windowed --name 复制搜索 main.py
+conda run -n rosetta --no-capture-output pyinstaller --noconfirm --windowed --name 复制搜索 main.py
+
+cd dist
+rm 复制搜索.zip
+7z a 复制搜索.zip 复制搜索.app
